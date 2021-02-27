a139e510f03e2ffabb5275b8ba7239a5b3dd29d5
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-02-27 08:16:36 +0530

    Add files via upload

---
+++ zip.py
@@ -0,0 +1,17 @@
+
+
+# importing required modules 
+from zipfile import ZipFile 
+  
+# specifying the zip file name 
+file_name = "Python--master.zip"
+  
+# opening the zip file in READ mode 
+with ZipFile(file_name, 'r') as zip: 
+    # printing all the contents of the zip file 
+    zip.printdir() 
+  
+    # extracting all the files 
+    print('Extracting all the files now...') 
+    zip.extractall() 
+    print('Done')
