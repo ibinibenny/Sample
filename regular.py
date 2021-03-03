b4ec8c951e36b0bfc1145c05e59f366e925b138a
ibinibenny <48010643+ibinibenny@users.noreply.github.com>
2021-03-03 08:54:59 +0530

    Update regular.py

--- regular.py
+++ regular.py
@@ -1,4 +1,20 @@
 
+re.search(pattern, string, flags=0)
+
+    Scan through string looking for the first location where the regular expression pattern produces a match, and return a corresponding match object. 
+Return None if no position in the string matches the pattern; note that this is different from finding a zero-length match at some point in the string.
+
+re.match(pattern, string, flags=0)
+
+    If zero or more characters at the beginning of string match the regular expression pattern, return a corresponding match object. 
+Return None if the string does not match the pattern; note that this is different from a zero-length match.
+
+ re.findall(pattern, string, flags=0)
+
+    Return all non-overlapping matches of pattern in string, as a list of strings. 
+    The string is scanned left-to-right, and matches are returned in the order found. If one or more groups are present in the pattern, return a list of groups; this will be a list of tuples if the pattern has more than one group. 
+  Empty matches are included in the result.
+
 
 # import re 
   
